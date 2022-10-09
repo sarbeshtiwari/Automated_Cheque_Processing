b37e684584ada7727b7e3b6fb71b1d3083583ebc
sarbeshtiwari <sarbeshtiwari39@gmail.com>
2022-10-09 13:02:16 +0530

    added mysql connection

--- ACP_home.py
+++ ACP_home.py
@@ -2,21 +2,26 @@ import tkinter
 from tkinter import *
 import tkinter.messagebox as mb
 import cv2
+import mysql.connector as ms
 
 w=tkinter.Tk()
 w.title("Automated Cheque Processing System")
 w.geometry("1350x600")
+w.config(bg='black')
+img=PhotoImage(file="IMG_20200309_181431 (1).png")
+label=Label(w,image=img)
+label.place(x=0,y=0)
 
 
 
-def Image_processing():
+def Image_processing():  #Capturing Image
   mb.showinfo("Alert",'Opening scaning windows')
   key = cv2. waitKey(1)
   webcam = cv2.VideoCapture(0)
   while True:
         try:
             check, frame = webcam.read()
-            print(frame) #prints matrix values of each framecd 
+            #print(frame) #prints matrix values of each frames 
             cv2.imshow("Capturing", frame)
             key = cv2.waitKey(1)
             cv2.imwrite(filename='saved_img.jpg', img=frame)
@@ -35,6 +40,7 @@ def Image_processing():
             print("Resized...")
             img_resized = cv2.imwrite(filename='saved_img-final.jpg', img=img_)
             print("Image saved!")
+            mb.showinfo("Alert",'Image Scanned proceed further')
             image_ml()
             break
             
@@ -45,13 +51,51 @@ def Image_processing():
             print("Program ended.")
             cv2.destroyroyAllWindows()
             break
+
+
 def image_ml():
-    print("Hello")
+    MyDB =ms.connect(host="localhost", user= "root", password="sarbesh", database="image_processing")
+    MyCursor = MyDB.cursor()
+    MyCursor.execute("CREATE TABLE IF NOT EXISTS `Images` ( `id` INTEGER UNSIGNED NOT NULL AUTO_INCREMENT, `Photo` LONGBLOB NOT NULL,PRIMARY KEY(`id`))")
+
+    
+    
+    def InsertBlob(FilePath): #insert image in mysql server/database
+       
+        with open(FilePath, "rb") as File:
+            BinaryData =File.read()
+        SQLStatement = "INSERT INTO Images (Photo) VALUES (%s)"
+        MyCursor.execute(SQLStatement, (BinaryData, ))
+        MyDB.commit()
+        
+
+    def RetrieveBlob(ID): # retrive image from mysql to local system
+        SQLStatement2= "SELECT * FROM Images WHERE id = '{0}'"
+        MyCursor.execute(SQLStatement2.format(str(ID)))
+        MyResult= MyCursor.fetchone()[1]
+        StoreFilePath ="Image/img{0}.jpg".format(str(ID))
+        print(MyResult)
+        with open(StoreFilePath, "wb") as File:
+            File.write(MyResult)
+            File.close()
+
+    print("1. Insert Image\n2. Read Image")
+    MenuInput =input()
+    if int(MenuInput) == 1:
+        UserFilePath =input("Enter File Path:")
+        InsertBlob(UserFilePath)
+    elif int(MenuInput) == 2:
+        UserIDChoice = input("Enter ID:")
+        RetrieveBlob(UserIDChoice)
+    else:
+        print("Byeeeee")
+
 
 def home():
     
-    Button(w, text='Continue', width=10, font=("Arial Bold",12),\
-       command=Image_processing).grid(row=1, column=1, sticky='w', pady=4)
+    button1=Button(w, text='Continue', width=10, font=("Arial Bold",12),\
+       command=Image_processing).grid(row=1, column=1, sticky='w', padx=120, pady=100)
+    
 
 home()
 
